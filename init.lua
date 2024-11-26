11b1c904b18a28f00f818967e7ab3d63e21d9d14
Bernat <delgadovidalbernat@gmail.com>
2024-11-26 20:25:52 +0100

    change tabnine accept key

--- init.lua
+++ init.lua
@@ -120,7 +120,7 @@ require 'lazy-plugins'
 -- Tabnine
 require('tabnine').setup {
   disable_auto_comment = true,
-  accept_keymap = '<C-Tab>',
+  accept_keymap = '<M-Tab>',
   dismiss_keymap = '<C-]>',
   debounce_ms = 800,
   suggestion_color = { gui = '#808080', cterm = 244 },
--- lua/custom/plugins/coc-nvim.lua
+++ lua/custom/plugins/coc-nvim.lua
@@ -1,7 +1,7 @@
--- return {
---   {
---     'neoclide/coc.nvim',
---     branch = 'release',
---     build = 'yarn install --frozen-lockfile',
---   },
--- }
+return {
+  {
+    --     'neoclide/coc.nvim',
+    --     branch = 'release',
+    --     build = 'yarn install --frozen-lockfile',
+  },
+}
