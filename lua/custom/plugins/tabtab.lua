734911e8251ae3e49ac07afbf4f01d2cd950d60a
Bernat <delgadovidalbernat@gmail.com>
2025-08-14 10:11:31 +0200

    update tabtab keybindings

--- lua/custom/plugins/tabtab.lua
+++ lua/custom/plugins/tabtab.lua
@@ -2,7 +2,9 @@ return {
   {
     'romgrk/barbar.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    init = function() vim.g.barbar_auto_setup = true end,
+    init = function()
+      vim.g.barbar_auto_setup = true
+    end,
     opts = {
       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
       animation = true,
@@ -15,22 +17,22 @@ return {
       local opts = { noremap = true, silent = true }
 
       -- Move to previous/next
-      map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-      map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+      map('n', '<Space>,', '<Cmd>BufferPrevious<CR>', opts)
+      map('n', '<Space>.', '<Cmd>BufferNext<CR>', opts)
       -- Re-order to previous/next
       map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
       map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
       -- Goto buffer in position...
-      map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-      map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-      map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-      map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-      map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-      map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-      map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-      map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-      map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-      map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+      map('n', '<Space>1', '<Cmd>BufferGoto 1<CR>', opts)
+      map('n', '<Space>2', '<Cmd>BufferGoto 2<CR>', opts)
+      map('n', '<Space>3', '<Cmd>BufferGoto 3<CR>', opts)
+      map('n', '<Space>4', '<Cmd>BufferGoto 4<CR>', opts)
+      map('n', '<Space>5', '<Cmd>BufferGoto 5<CR>', opts)
+      map('n', '<Space>6', '<Cmd>BufferGoto 6<CR>', opts)
+      map('n', '<Space>7', '<Cmd>BufferGoto 7<CR>', opts)
+      map('n', '<Space>8', '<Cmd>BufferGoto 8<CR>', opts)
+      map('n', '<Space>9', '<Cmd>BufferGoto 9<CR>', opts)
+      map('n', '<Space>0', '<Cmd>BufferLast<CR>', opts)
       -- Pin/unpin buffer
       map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
       -- Close buffer
@@ -50,6 +52,6 @@ return {
       map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
       map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
       map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
-    end
+    end,
   },
 }
